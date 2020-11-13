8be9ecdf7c4446ae16105419156ea78f8509ee5b
RYBNetWork <constancechronicles@gmail.com>
2020-11-12 19:48:06 -0500

    Update call_api.js
    updated connection string syntax.

--- call_api.js
+++ call_api.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 function call_api(finishedAPI, ticker) {
-	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=API_KEY', { json: true }, (err, res, body) => {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=' + API_KEY, { json: true }, (err, res, body) => {
 	if (err) {return console.log(err);}
 	if (res.statusCode === 200){
 			finishedAPI(body);
