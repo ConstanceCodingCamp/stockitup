945574d70f3420a903368d9520a26a70eb081d50
RYBNetWork <constancechronicles@gmail.com>
2020-11-12 19:47:27 -0500

    Update index.js
    updated connection string with correct syntax.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended:false}));
 
 function call_api(finishedAPI, ticker) {
-	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=API_KEY', { json: true }, (err, res, body) => {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=' + API_KEY, { json: true }, (err, res, body) => {
 	if (err) {return console.log(err);}
 	if (res.statusCode === 200){
 			finishedAPI(body);
